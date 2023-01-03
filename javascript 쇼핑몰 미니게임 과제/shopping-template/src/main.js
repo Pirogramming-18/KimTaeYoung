982fcc53e6dc688690f557cfa60de28afeddd040
rlaxodud1999@gmail.com <rlaxodud1999gmail.com>
2023-01-03 19:25:48 +0900

    display items

--- javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
+++ javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
@@ -4,10 +4,24 @@ function loadItems() {
 		.then((json) => json.items);
 }
 
+function displayItems(items) {
+	const container = document.querySelector(".items");
+	container.innerHTML = items.map((item) => creatHTMLString(item)).join("");
+}
+
+function creatHTMLString(item) {
+	return `
+  <li class="item">
+      <img src=${item.image} alt=${item.type} class="item__thumbnail" />
+      <span class="item__description">${item.gender}, ${item.size}</span>
+  </li>
+  `;
+}
+
 loadItems()
 	.then((items) => {
 		console.log(items);
-		//displayItems(items);
-		//setEventListeners(items);
+		displayItems(items);
+		// setEventListeners(items);
 	})
 	.catch(console.log);
