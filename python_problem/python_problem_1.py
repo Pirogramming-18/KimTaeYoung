d3fb11a9aa632fce93f42a053d4415d3cae6594c
rlaxodud1999@gmail.com <rlaxodud1999gmail.com>
2023-01-06 21:41:50 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,23 +1,25 @@
-
+import random
 
 num = 0
-player1 = "PlayerA"
-player2 = "playerB"
+player1 = "player"
+player2 = "computer"
 isPlayerTurn = True
 
 
 def brGame():
   global num
   global isPlayerTurn
-  
-  try:
-    call = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-  except ValueError:
-    print("정수를 입력하세요")
-    return
-  if call <= 0 or call > 3:
-    print("1,2,3 중 하나를 입력하세요")
-    return
+  if isPlayerTurn:
+    try:
+      call = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+    except ValueError:
+      print("정수를 입력하세요")
+      return
+    if call <= 0 or call > 3:
+      print("1,2,3 중 하나를 입력하세요")
+      return
+  else:
+    call = random.randint(1,3)
   
   for i in range(call):
     
