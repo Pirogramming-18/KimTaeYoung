139b52e0f6ed568959efb760a3cbdd6c54d7f3f9
rlaxodud1999@gmail.com <rlaxodud1999gmail.com>
2023-01-06 21:33:52 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -4,18 +4,20 @@ num = 0
 player1 = "PlayerA"
 player2 = "playerB"
 isPlayerTurn = True
-while 1:
 
-  
+
+def brGame():
+  global num
+  global isPlayerTurn
 
   try:
     call = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
   except ValueError:
     print("정수를 입력하세요")
-    continue
+    return
   if call <= 0 or call > 3:
     print("1,2,3 중 하나를 입력하세요")
-    continue
+    return
   
   for i in range(call):
     
@@ -24,10 +26,15 @@ while 1:
     if num == 31:
       
       print(f'{player2} win!') if isPlayerTurn == True else print(f'{player1} win!')
-      break
+      return
+
+
+while 1:
+
+  brGame()
+
   if num == 31:
     break
-  
 
   isPlayerTurn = not isPlayerTurn
   
