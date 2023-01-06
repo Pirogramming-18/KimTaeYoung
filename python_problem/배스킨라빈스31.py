bb8b93b2edf32990ee7032ee5e93151e5c23dc3c
rlaxodud1999@gmail.com <rlaxodud1999gmail.com>
2023-01-06 16:47:40 +0900

    배스킨라빈스31 9단계

--- python_problem/배스킨라빈스31.py
+++ python_problem/배스킨라빈스31.py
@@ -10,22 +10,26 @@ def brGame():
   player2 = "computer"
   
 
-  try:
-    call = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-  except:
-    print("정수를 입력하세요")
-    return
+  if isPlayer1Turn:
+    try:
+      call = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+    except:
+      print("정수를 입력하세요")
+      return
   
-  if (call == 0 or call > 3):
-    print("1,2,3 중 하나를 입력하세요")
-    return
-  
+    if (call == 0 or call > 3):
+      print("1,2,3 중 하나를 입력하세요")
+      return
+  else:
+    call = random.randint(1, 3)
   for _ in range(call):
     num+= 1
     if num == 31:
       print(f'{player2} win!') if isPlayer1Turn == True else print(f'{player1} win!')
       return
     print(f'{player1}: {num}') if isPlayer1Turn == True else print(f'{player2}: {num}')
+  
+  isPlayer1Turn = not isPlayer1Turn
 
 
     
@@ -43,5 +47,6 @@ while 1:
 
   if num == 31:
     break
-  isPlayer1Turn = ~isPlayer1Turn
+
+  
   
