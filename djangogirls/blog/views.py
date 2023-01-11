9f411a9a1e3194c1b1d268961b64105efe44fde7
rlaxodud1999@gmail.com <rlaxodud1999gmail.com>
2023-01-11 14:09:56 +0900

    Django 템플릿

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -5,5 +5,6 @@ from .models import Post
 
 def post_list(request):
     
-    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
+    return render(request, 'blog/post_list.html', {'posts': posts})
